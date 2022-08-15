e64672b4bdd5431dcd653bed14b6e01e233b32ec
Joshua Ninan Valson <joshuavalson@gmail.com>
2022-08-15 10:32:31 +0530

    Create Roles.java

---
+++ emsapp/emsapp/src/main/java/com/greatlearning/emsapp/model/Roles.java
@@ -0,0 +1,54 @@
+package com.greatlearning.emsapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Roles {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "username")
+	private String userName;
+
+	@Column(name = "role")
+	private String role;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Roles [id=" + id + ", userName=" + userName + ", role=" + role + "]";
+	}
+	
+	
+}
